3a2e92f5f6050a1c9aa1cdc131dddec1f1eef5a0
Giuseppe Corti <gsc.rul3z@gmail.com>
2013-06-02 16:47:26 +0200

    added connection/file check

--- oneslip.py
+++ oneslip.py
@@ -17,13 +17,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import WebKit2, Gtk
+from gi.repository import WebKit2, Gtk, GObject
 
 import sys
 import string
 import os
+import urllib2
 
 COOKIEDIR = os.getenv('HOME') + "/.cookie/cookies.txt"
+RESDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./resources")
 
 class GUI():
 	def __init__(self, donotshow=False):
@@ -34,10 +36,9 @@ class GUI():
 
 		# Cookie support
 		context = self.view.get_context()
-		self.cookie = context.get_cookie_manager()
-		self.cookie.set_persistent_storage(COOKIEDIR,WebKit2.CookiePersistentStorage.TEXT)
-		self.cookie.set_accept_policy(WebKit2.CookieAcceptPolicy.ALWAYS)
-
+		cookie = context.get_cookie_manager()
+		cookie.set_persistent_storage(COOKIEDIR,WebKit2.CookiePersistentStorage.TEXT)
+		cookie.set_accept_policy(WebKit2.CookieAcceptPolicy.ALWAYS)
 
 		self.view.load_uri(sys.argv[1])
 		
@@ -47,9 +48,9 @@ class GUI():
 		self.window.show_all()
 		self.view.connect("load-changed", self.load_finished)
 		self.view.connect("load-failed", self.load_failed)
-
-
+		
 		self.window.connect('delete-event', lambda window, event: Gtk.main_quit())
+
 
 	def load_failed(self, view, frame):
 		self.window.set_title("Error!")
@@ -64,13 +65,37 @@ class GUI():
 			self.window.set_title(title)
 
 if __name__ == "__main__":
+
+	def internet_on(page):
+		try:
+			response=urllib2.urlopen(page,timeout=1)
+			return True
+		except urllib2.URLError as err: pass
+		return False
+
 	if len(sys.argv) < 3:
 		sys.exit("Usage: %s (URL) (width)x(height)" % sys.argv[0])
 
+	# decode size
 	size = string.split(sys.argv[2],'x')
+	# decode protocol
+	protocol = sys.argv[1][:4] 
 
+	# Verify size
 	if size[0].isdigit() == False or size[1].isdigit() == False:
 		sys.exit("%s isn't a valid size" % sys.argv[2])
+
+	# check protocol
+	if protocol == "file":
+		# Check if file exist
+		if os.path.exists(sys.argv[1][7:])==False:
+			sys.argv[1] = "file://" + RESDIR + "/errors/nofile.html" # Web application not found
+
+	else:
+		# Check if server is reachable
+		if internet_on(sys.argv[1])==False:
+			print "no connection"
+			sys.argv[1] = "file://" + RESDIR + "/errors/noconnection.html" # No network connection
 
 	g = GUI()
 	Gtk.main()
