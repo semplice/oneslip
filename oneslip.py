9bb3c7241bfe28a7847531149ffa891d6db657bb
Giuseppe Corti <gsc.rul3z@gmail.com>
2013-07-02 17:02:56 +0200

    modular code

--- oneslip.py
+++ oneslip.py
@@ -19,50 +19,14 @@
 
 from gi.repository import WebKit2, Gtk, GObject
 
+import classes.GUI
+
 import sys
 import string
 import os
 import urllib2
 
-COOKIEDIR = os.getenv('HOME') + "/.cookie/cookies.txt"
 APPDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./applications")
-
-class GUI():
-	def __init__(self, donotshow=False):
-
-		self.window = Gtk.Window()
-		#self.window.set_resizable(False)
-		self.view = WebKit2.WebView()
-
-		# Cookie support
-		context = self.view.get_context()
-		cookie = context.get_cookie_manager()
-		cookie.set_persistent_storage(COOKIEDIR,WebKit2.CookiePersistentStorage.TEXT)
-		cookie.set_accept_policy(WebKit2.CookieAcceptPolicy.ALWAYS)
-
-		self.view.load_uri(sys.argv[1])
-		
-		self.window.add(self.view)
-		self.view.set_size_request(int(size[0]), int(size[1]))
-
-		self.window.show_all()
-		self.view.connect("load-changed", self.load_finished)
-		self.view.connect("load-failed", self.load_failed)
-		
-		self.window.connect('delete-event', lambda window, event: Gtk.main_quit())
-
-
-	def load_failed(self, view, frame):
-		self.window.set_title("Error!")
-		#self.view.load.uri()
-
-	def load_finished(self, view, frame):
-		title = self.view.get_title()
-		if not title:
-			title = self.view.get_uri()
-		if title:
-			# Set web page title as Gtk Window title
-			self.window.set_title(title)
 
 if __name__ == "__main__":
 
@@ -97,5 +61,5 @@ if __name__ == "__main__":
 			#print "no connection"
 			sys.argv[1] = "file://" + APPDIR + "/messages/errors/noconnection.html" # No network connection
 
-	g = GUI()
+	g = classes.GUI.GUI(size[0],size[1],sys.argv[1])
 	Gtk.main()
